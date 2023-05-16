a1cbcd4e18f526c3852bebc5c0866e4f9529089f
Syed Hassanul Haque <lnemails003@gmail.com>
2023-05-17 01:27:46 +0530

    Updates

---
+++ precache-manifest.a02a714c8af94ec6ee2667a2004204ab.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fdbcd2e1a925cc9afa5",
+    "url": "/static/css/main.5ea1a8a7.chunk.css"
+  },
+  {
+    "revision": "2fdbcd2e1a925cc9afa5",
+    "url": "/static/js/main.2fdbcd2e.chunk.js"
+  },
+  {
+    "revision": "05e2b019d5e78b5c6929",
+    "url": "/static/js/1.05e2b019.chunk.js"
+  },
+  {
+    "revision": "dee2283e60c535e47c50",
+    "url": "/static/js/2.dee2283e.chunk.js"
+  },
+  {
+    "revision": "542a0c8012a9b736e622",
+    "url": "/static/js/runtime~main.542a0c80.js"
+  },
+  {
+    "revision": "e80ee4ce0f483b7ddcd751150a2966fa",
+    "url": "/static/media/logo.e80ee4ce.jpeg"
+  },
+  {
+    "revision": "4220802ef4140ed2a5dd731812ed6386",
+    "url": "/static/media/php.4220802e.png"
+  },
+  {
+    "revision": "f8f2dacaf150cb572983418391b4a07f",
+    "url": "/static/media/java.f8f2daca.png"
+  },
+  {
+    "revision": "79d6bc275a8a34b0219379c3df78382a",
+    "url": "/static/media/c.79d6bc27.png"
+  },
+  {
+    "revision": "c73dfa3f2f977cb2913004f16efcef10",
+    "url": "/static/media/cpp.c73dfa3f.png"
+  },
+  {
+    "revision": "30612f2d72adda4093ce215bd0102f64",
+    "url": "/static/media/mysql.30612f2d.png"
+  },
+  {
+    "revision": "ad5f57a203214de510d040150baf52f1",
+    "url": "/static/media/vscode.ad5f57a2.png"
+  },
+  {
+    "revision": "4f988c9e9c25ee3728a0aba55d3c4d94",
+    "url": "/static/media/react.4f988c9e.png"
+  },
+  {
+    "revision": "9b4ef810eaf9affb49e60dc74f7a9f9a",
+    "url": "/static/media/android.9b4ef810.png"
+  },
+  {
+    "revision": "32b6fc12260c96f711150637f382841f",
+    "url": "/static/media/html5.32b6fc12.png"
+  },
+  {
+    "revision": "7c12764796349bfb505d4c5d73489c3d",
+    "url": "/static/media/css3.7c127647.png"
+  },
+  {
+    "revision": "9ee214f1688539a4f1edb45ae5775cde",
+    "url": "/static/media/card_bg.9ee214f1.jpg"
+  },
+  {
+    "revision": "178627e3517fe460c1e10f2697bf952e",
+    "url": "/static/media/javascript.178627e3.png"
+  },
+  {
+    "revision": "9440ae5d4065737ce14f3d325d6dee4b",
+    "url": "/index.html"
+  }
+];
